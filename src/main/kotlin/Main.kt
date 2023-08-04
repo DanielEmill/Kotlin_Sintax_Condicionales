e59141ba62fcda9e63d3063658ba4c8a387659b3
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-04 12:51:20 -0400

    Uso de If/else

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    println(52<1)
+    println("Puertas de colores:")
+    val Color:String = "Azul"
+    if(Color == "Blanco")
+    {
+        println("Entraste a la puerta blanca.")
+    }
+    else if(Color == "Negro")
+    {
+        println("Entraste a la puerta negra.")
+    }
+    else
+    {
+        println("No entraste a ninguna de la dos puerta.")
+    }
+
 }
