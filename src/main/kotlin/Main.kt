97cf4a91f61926481d95b5f95b5529378100a4a1
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-04 13:00:16 -0400

    Uso de When

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,7 +17,15 @@ fun main() {
         "Negro" -> println("Entraste a la puerta negra.")
         "Blanco" -> println("Entraste a la puerta blanca.")
         else -> println("Colores invalidos")
+    }
 
+    val x: Any = 4
+
+    when (x) {
+        2, 3, 5, 7 -> println("X es un numero primo entre el 1 al 10")
+        in 1..10 -> println("X es un numero entre el 1 y el 10 pero no es primo.")
+        is Int -> println("x es entero.")
+        else -> println("x no es un entero.")
     }
 
 }
